964b17593d43d542f4983fc6fc1d8125e611ddd2
Thanh Vinh <ptvinh203@gmail.com>
2024-07-24 21:27:36 +0700

    feat: audio recording

--- client/src/components/Chat/MessageBar.jsx
+++ client/src/components/Chat/MessageBar.jsx
@@ -9,6 +9,9 @@ import { FaMicrophone } from "react-icons/fa";
 import { ImAttachment } from "react-icons/im";
 import { MdSend } from "react-icons/md";
 import PhotoPicker from "../common/PhotoPicker";
+import dynamic from "next/dynamic";
+
+const CaptureAudio = dynamic(() => import("../common/CaptureAudio"), { ssr: false });
 
 function MessageBar() {
   const [{ userInfo, currentChatUser, socket }, dispatch] = useStateProvider();
@@ -107,46 +110,56 @@ function MessageBar() {
 
   return (
     <div className="bg-panel-header-background h-20 px-4 flex items-center gap-6 relative">
-      <>
-        <div className="flex gap-6">
-          <BsEmojiSmile
-            className="text-panel-header-icon cursor-pointer text-xl"
-            title="Emoji"
-            id="emoji-open"
-            onClick={handleEmojiModal}
-          />
-          {showEmojiPicker && (
-            <div className="absolute bottom-24 left-16 z-40" ref={emojiPickerRef}>
-              <EmojiPicker onEmojiClick={handleEmojiClick} theme="dark" />
-            </div>
-          )}
-          <ImAttachment
-            className="text-panel-header-icon cursor-pointer text-xl"
-            title="Attach File"
-            onClick={() => setGrabPhoto(true)}
-          />
-        </div>
-        <div className="w-full rounded-lg h-10 flex items-center">
-          <input
-            type="text"
-            placeholder="Type a message"
-            className="bg-input-background text-sm focus:outline-none text-white h-10 rounded-lg px-5 py-4 w-full"
-            onChange={(e) => setMessage(e.target.value)}
-            value={message}
-          />
-        </div>
-        <div className="flex w-10 items-center justify-center">
-          <button>
-            <MdSend
+      {!showAudioRecorder && (
+        <>
+          <div className="flex gap-6">
+            <BsEmojiSmile
               className="text-panel-header-icon cursor-pointer text-xl"
-              title="Send message"
-              onClick={sendMessage}
+              title="Emoji"
+              id="emoji-open"
+              onClick={handleEmojiModal}
             />
-            {/* <FaMicrophone className="text-panel-header-icon cursor-pointer text-xl" title="Record" /> */}
-          </button>
-        </div>
-      </>
+            {showEmojiPicker && (
+              <div className="absolute bottom-24 left-16 z-40" ref={emojiPickerRef}>
+                <EmojiPicker onEmojiClick={handleEmojiClick} theme="dark" />
+              </div>
+            )}
+            <ImAttachment
+              className="text-panel-header-icon cursor-pointer text-xl"
+              title="Attach File"
+              onClick={() => setGrabPhoto(true)}
+            />
+          </div>
+          <div className="w-full rounded-lg h-10 flex items-center">
+            <input
+              type="text"
+              placeholder="Type a message"
+              className="bg-input-background text-sm focus:outline-none text-white h-10 rounded-lg px-5 py-4 w-full"
+              onChange={(e) => setMessage(e.target.value)}
+              value={message}
+            />
+          </div>
+          <div className="flex w-10 items-center justify-center">
+            <button>
+              {message.length ? (
+                <MdSend
+                  className="text-panel-header-icon cursor-pointer text-xl"
+                  title="Send message"
+                  onClick={sendMessage}
+                />
+              ) : (
+                <FaMicrophone
+                  className="text-panel-header-icon cursor-pointer text-xl"
+                  title="Record"
+                  onClick={() => setShowAudioRecorder(true)}
+                />
+              )}
+            </button>
+          </div>
+        </>
+      )}
       {grabPhoto && <PhotoPicker onChange={photoPickerChange} />}
+      {showAudioRecorder && <CaptureAudio setShowAudioRecorder={setShowAudioRecorder} />}
     </div>
   );
 }
--- client/src/components/common/CaptureAudio.jsx
+++ client/src/components/common/CaptureAudio.jsx
@@ -1,7 +1,185 @@
-import React from "react";
+import { useStateProvider } from "@/context/StateContext";
+import React, { useEffect, useRef, useState } from "react";
+import { FaMicrophone, FaPauseCircle, FaPlay, FaStop, FaTrash } from "react-icons/fa";
+import { MdSend } from "react-icons/md";
+import WaveSurfer from "wavesurfer.js";
 
-function CaptureAudio() {
-  return <div>CaptureAudio</div>;
+function CaptureAudio({ setShowAudioRecorder }) {
+  const [{ userInfo, currentChatUser, socket }, dispatch] = useStateProvider();
+
+  const [isRecording, setIsRecording] = useState(false);
+  const [recoredAudio, setRecordedAudio] = useState(null);
+  const [waveform, setWaveform] = useState(null);
+  const [recordingDuration, setRecordingDuration] = useState(0);
+  const [currentPlaybackTime, setCurrentPlaybackTime] = useState(0);
+  const [totalDuration, setTotalDuration] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [renderedAudio, setRenderedAudio] = useState(null);
+
+  const audioRef = useRef(null);
+  const mediaRef = useRef(null);
+  const waveFormRef = useRef(null);
+
+  useEffect(() => {
+    let interval;
+    if (isRecording) {
+      interval = setInterval(() => {
+        setRecordingDuration((prevDuration) => {
+          setTotalDuration(prevDuration + 1);
+          return prevDuration + 1;
+        });
+      }, 1000);
+    }
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isRecording]);
+
+  useEffect(() => {
+    const wavesurfer = WaveSurfer.create({
+      container: waveFormRef.current,
+      waveColor: "#ccc",
+      progressColor: "#4a9eff",
+      cursorColor: "#7ae3c3",
+      barWidth: 2,
+      height: 30,
+      responsive: true,
+    });
+    setWaveform(wavesurfer);
+
+    wavesurfer.on("finish", () => {
+      setIsPlaying(false);
+    });
+
+    return () => {
+      wavesurfer.destroy();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (waveform) handleStartRecording();
+  }, [waveform]);
+
+  useEffect(() => {
+    if (recoredAudio) {
+      const updatePlaybackTime = () => {
+        setCurrentPlaybackTime(recoredAudio.currentTime);
+      };
+      recoredAudio.addEventListener("timeupdate", updatePlaybackTime);
+      return () => {
+        recoredAudio.removeEventListener("timeupdate", updatePlaybackTime);
+      };
+    }
+  }, [renderedAudio]);
+
+  const handlePlayRecording = () => {
+    if (recoredAudio) {
+      waveform.stop();
+      waveform.play();
+      recoredAudio.play();
+      setIsPlaying(true);
+    }
+  };
+  const handlePauseRecording = () => {
+    waveform.stop();
+    recoredAudio.pause();
+    setIsPlaying(false);
+  };
+
+  const handleStartRecording = () => {
+    setRecordingDuration(0);
+    setCurrentPlaybackTime(0);
+    setTotalDuration(0);
+    setIsRecording(true);
+    navigator.mediaDevices
+      .getUserMedia({ audio: true })
+      .then((stream) => {
+        const mediaRecorder = new MediaRecorder(stream);
+        mediaRef.current = mediaRecorder;
+        audioRef.current.srcObject = stream;
+
+        const chunks = [];
+        mediaRecorder.ondataavailable = (e) => chunks.push(e.data);
+        mediaRecorder.onstop = () => {
+          const blob = new Blob(chunks, { type: "audio/ogg; codecs=opus" });
+          const audioURL = URL.createObjectURL(blob);
+          const audio = new Audio(audioURL);
+          setRecordedAudio(audio);
+
+          waveform.load(audioURL);
+        };
+
+        mediaRecorder.start();
+      })
+      .catch((err) => {
+        console.log("Error accessing microphone: ", err);
+      });
+  };
+  const handleStopRecording = () => {
+    if (mediaRef.current && isRecording) {
+      mediaRef.current.stop();
+      setIsRecording(false);
+      waveform.stop();
+
+      const audioChunks = [];
+      mediaRef.current.addEventListener("dataavailable", (event) => {
+        audioChunks.push(event.data);
+      });
+
+      mediaRef.current.addEventListener("stop", () => {
+        const audioBlob = new Blob(audioChunks, { type: "audio/mp3" });
+        const audioFile = new File([audioBlob], "recording.mp3");
+        setRenderedAudio(audioFile);
+      });
+    }
+  };
+
+  const sendRecording = async () => {};
+
+  const formatTime = (time) => {
+    if (isNaN(time)) return "00:00";
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    return `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+  };
+
+  return (
+    <div className="flex text-2xl w-full justify-end items-center">
+      <div className="pt-1">
+        <FaTrash className="text-panel-header-icon cursor-pointer" onClick={() => setShowAudioRecorder(false)} />
+      </div>
+      <div className="mx-4 py-2 px-4 text-white text-lg flex gap-3 justify-center items-center bg-search-input-container-background rounded-full drop-shadow-lg">
+        {isRecording ? (
+          <div className="text-red-500 animate-pulse w-60 text-center">
+            Recording <span>{recordingDuration}</span>
+          </div>
+        ) : (
+          <div>
+            {recoredAudio && (
+              <div className="cursor-pointer">
+                {!isPlaying ? <FaPlay onClick={handlePlayRecording} /> : <FaStop onClick={handlePauseRecording} />}
+              </div>
+            )}
+          </div>
+        )}
+        <div className="w-60" ref={waveFormRef} hidden={isRecording} />
+        {recoredAudio && isPlaying && <span>{formatTime(currentPlaybackTime)}</span>}
+        {recoredAudio && !isPlaying && <span>{formatTime(totalDuration)}</span>}
+        <audio ref={audioRef} hidden />
+        <div className="mr-4 cursor-pointer">
+          {!isRecording ? (
+            <FaMicrophone className="text-red-500" onClick={handleStartRecording} />
+          ) : (
+            <FaPauseCircle className="text-red-500" onClick={handleStopRecording} />
+          )}
+        </div>
+        <div>
+          <MdSend className="text-panel-header-icon cursor-pointer mr-4" title="Send" onClick={sendRecording} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CaptureAudio;
